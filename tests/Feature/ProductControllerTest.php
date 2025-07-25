13d960195e8d92dbbfe24d309bc2d6f202d67342
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-07-25 12:06:32 +0200

    test: complete functional tests for ProductController

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -37,6 +37,8 @@ class ProductControllerTest extends TestCase
 
     public function test_product_controller_store()
     {
+        // Store DB length in a variable
+        $productsCount = count(Product::all());
         // Simulate a user creating a new product through the web interface
         $response = $this->post(route('products.store'), [
             'name' => 'Sample Product Name',
@@ -46,9 +48,84 @@ class ProductControllerTest extends TestCase
         ]);
 
         // Assert that the post is successfully stored in the database
-        $this->assertCount(1, Product::all());
+        $this->assertCount($productsCount + 1, Product::all());
 
-        // Assert that the user is redirected to the Posts Index page after post creation
+        // Assert that the user is redirected to the products index page after
+        // product creation
         $response->assertRedirect(route('products.index'));
+    }
+
+    public function test_product_controller_store_validation_error()
+    {
+        // Simulate a user creating a new product with a blank name
+        $response = $this->post(route('products.store'), [
+            'name' => '',
+            'description' => 'Sample Product Description',
+            'price' => 65.99,
+            'stock' => 15,
+        ]);
+
+        // check if there is an error
+        $response->assertSessionHasErrors();
+    }
+
+    public function test_product_controller_update()
+    {
+        // Simulate a user creating a new product through the web interface
+        $this->post(route('products.store'), [
+            'name' => 'Sample Product Name',
+            'description' => 'Sample Product Description',
+            'price' => 65.99,
+            'stock' => 15,
+        ]);
+
+        // Retrieve ID from new product
+        $productId = Product::latest()->first()->id;
+
+        // update the product with new infos
+        $response = $this->put(route('products.update', $productId), [
+            'name' => 'Updated Product Name',
+            'description' => 'Updated Product Description',
+            'price' => 665.99,
+            'stock' => 2,
+        ]);
+
+        // Assert that the user is redirected to the products index page after
+        // product update
+        $response->assertRedirect(route('products.index'));
+
+        // Assert that updated datas are in the DB
+        $this->assertDatabaseHas('products', [
+            'id' => $productId,
+            'name' => 'Updated Product Name',
+            'description' => 'Updated Product Description',
+            'price' => 665.99,
+            'stock' => 2,
+        ]);
+    }
+
+    public function test_product_controller_destroy()
+    {
+        // Simulate a user creating a new product through the web interface
+        $this->post(route('products.store'), [
+            'name' => 'Sample Product Name',
+            'description' => 'Sample Product Description',
+            'price' => 65.99,
+            'stock' => 15,
+        ]);
+
+        // Retrieve ID from new product
+        $product = Product::latest()->first();
+
+        // update the product with new infos
+        $response = $this->delete(route('products.destroy', $product));
+
+        // Assert that the user is redirected to the products index page after
+        // product delete
+        $response->assertRedirect(route('products.index'));
+
+        // Assert that product is not displayed
+        $response->assertDontSee('Sample Product Name');
+
     }
 }
